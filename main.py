1a4e21f6512b71b6ac1cbd7b89fcb2f6404a19f0
Def0ltT3pe <svoboda2076@mail.ru>
2025-04-12 03:52:26 +0300

    fix: passer aliexpress

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
 from parser_wb import get_wb_price
+from parser_aliexpress import get_aliexpress_data
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
-# Инициализация FastAPI
 app = FastAPI()
-
-# Создание пула потоков
 executor = ThreadPoolExecutor(max_workers=5)
-
 
 @app.get("/parse/wb")
 async def parse_wb(nm_id: str):
     loop = asyncio.get_event_loop()
-
-    # Асинхронный вызов парсера в отдельном потоке
     price = await loop.run_in_executor(executor, get_wb_price, nm_id)
     return {"nm_id": nm_id, "price": price}
+
+
+@app.get("/parse/aliexpress")
+async def parse_aliexpress(url: str):
+    loop = asyncio.get_event_loop()
+    data = await loop.run_in_executor(executor, get_aliexpress_data, url)
+    return data
---
+++ parser_aliexpress.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from urllib.parse import urlparse, parse_qs
+
+def get_aliexpress_data(url: str):
+    # Укажите путь к geckodriver
+    service = Service("D:/Konina/geckodriver-v0.36.0-win64/geckodriver.exe")  # Обрати внимание: слэши правильные
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--headless')  # Без интерфейса
+
+    driver = webdriver.Firefox(service=service, options=options)
+    driver.get(url)
+
+    # Парсим URL и получаем sku_id
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+    sku_id = query_params.get('sku_id', [None])[0]
+
+    # Имя товара
+    try:
+        name = driver.find_element(By.CSS_SELECTOR, 'h1.snow-ali-kit_Typography__base__1shggo').text
+    except Exception as e:
+        name = f"Ошибка: {e}"
+
+    # Цена
+    try:
+        price = driver.find_element(By.CSS_SELECTOR, '.HazeProductPrice_SnowPrice__mainS__1wzo3').text
+    except Exception as e:
+        price = f"Ошибка: {e}"
+
+    driver.quit()
+
+    return {
+        "sku_id": sku_id,
+        "name": name,
+        "price": price
+    }
