8505583fd442978701145d5e9e79dd7cf6a266c3
Def0ltT3pe <svoboda2076@mail.ru>
2025-04-13 13:40:04 +0300

    Fix: parser ali

--- parser_aliexpress.py
+++ parser_aliexpress.py
@@ -2,20 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from urllib.parse import urlparse, parse_qs
+import re
 
 def get_aliexpress_data(url: str):
-    # Укажите путь к geckodriver
-    service = Service("D:\\Konina\\geckodriver-v0.36.0-win64\\geckodriver.exe")  # Обрати внимание: слэши правильные
+    service = Service("C:\\Users\\svobo\\Documents\\GitHub\\Konina\\geckodriver-v0.36.0-win64\\geckodriver.exe")
     options = webdriver.FirefoxOptions()
-    options.add_argument('--headless')  # Без интерфейса
+    #options.add_argument('--headless')
 
     driver = webdriver.Firefox(service=service, options=options)
     driver.get(url)
 
-    # Парсим URL и получаем sku_id
+    # Парсим URL и получаем sku_id, если есть
     parsed_url = urlparse(url)
     query_params = parse_qs(parsed_url.query)
     sku_id = query_params.get('sku_id', [None])[0]
+
+    # Если sku_id не найден, ищем его в исходном коде страницы
+    if not sku_id:
+        try:
+            page_source = driver.page_source
+            match = re.search(r'"skuId"\s*:\s*"(\d+)"', page_source)
+            if match:
+                sku_id = match.group(1)
+        except:
+            sku_id = "0"
 
     # Имя товара
     try:
@@ -38,5 +48,7 @@ def get_aliexpress_data(url: str):
         "price": price,
         "marketplace": "Aliexpress"
     }
-
-
+if __name__ == "__main__":
+    url = "https://aliexpress.ru/item/1005008216442145.html?spm=a2g2w.home.10009201.3.6f135586Ue6SPj&mixer_rcmd_bucket_id=aerabtestalgoRecommendAbV2_testRankingTimLialikov&ru_algo_pv_id=672037-55914f-9d1519-9725ba-1744534800&scenario=aerAppJustForYouNewRuSellTab&sku_id=12000044268637948&traffic_source=recommendation&type_rcmd=core"
+    data = get_aliexpress_data(url)
+    print(data)
